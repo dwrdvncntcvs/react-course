1d54f10243c84a054801d3b3fa079bdde8d1886d
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-09-23 11:46:52 +0800

    All about useEffect

--- Section 10/advance-react/src/App.js
+++ Section 10/advance-react/src/App.js
@@ -1,19 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const isAuthenticated = localStorage.getItem("isAuthenticated");
+
+    if (isAuthenticated) setIsLoggedIn((prev) => (prev = isAuthenticated));
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isAuthenticated", true);
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("isAuthenticated");
     setIsLoggedIn(false);
   };
 
