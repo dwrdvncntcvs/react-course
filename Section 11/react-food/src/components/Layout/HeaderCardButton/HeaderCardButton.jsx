1f1c065d736a3f06d5814938e15f0ff23ac5fdd8
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-09-29 19:58:51 +0800

    Side effect in button

--- Section 11/react-food/src/components/Layout/HeaderCardButton/HeaderCardButton.jsx
+++ Section 11/react-food/src/components/Layout/HeaderCardButton/HeaderCardButton.jsx
@@ -1,15 +1,34 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import { useCartContext } from "../../../hooks/cartHooks";
 import CartIcon from "../../Cart/CartIcon/CartIcon";
 import style from "./headerCardButton.module.css";
 
 const HeaderCardButton = ({ onClick }) => {
+  const [buttonBump, setButtonBump] = useState(false);
   const { items } = useCartContext();
 
   const numberOfItems = items.reduce((prev, cur) => prev + cur.amount, 0);
 
+  const btnClasses = `${style.button} ${buttonBump ? style.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length < 1) return;
+
+    setButtonBump(true);
+
+    const timeout = setTimeout(() => {
+      setButtonBump(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [items]);
+
   return (
-    <button className={style.button} onClick={onClick}>
+    <button className={btnClasses} onClick={onClick}>
       <span className={style.icon}>
         <CartIcon />
       </span>
