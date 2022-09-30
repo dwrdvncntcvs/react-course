5eee7988494d43cd76d6482ecaa1135245b62f82
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-09-30 13:07:42 +0800

    Minor changes

--- Section 12/section-12/src/App.js
+++ Section 12/section-12/src/App.js
@@ -6,17 +6,25 @@ import Button from "./components/UI/Button/Button";
 
 function App() {
   const [showP, setShowP] = useState(false);
+  const [allow, setAllow] = useState(false);
 
   console.count("APP RUNNING...");
 
   const showParagraphAction = useCallback(() => {
+    if (!allow) return;
+
     setShowP((prev) => !prev);
-  }, []);
+  }, [allow]);
+
+  const allowToggling = () => {
+    setAllow((prev) => !prev);
+  };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
       <Demo show={showP} />
+      <Button onClick={allowToggling}>Allow Toggle</Button>
       <Button onClick={showParagraphAction}>Show Paragraph</Button>
     </div>
   );
