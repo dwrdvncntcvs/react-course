c91d31cc22dd0b5d9c0b10c19f6e8ed5a600a1d6
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-10-06 18:01:31 +0800

    Creating post request to post and get data from firebase

--- Section 14/react-app/src/App.js
+++ Section 14/react-app/src/App.js
@@ -1,77 +1,90 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
 import "./App.css";
 
-const BASE_URL = "https://swapi.dev/api";
-
-// const dummyMovies = [
-//   {
-//     id: 1,
-//     title: 'Some Dummy Movie',
-//     openingText: 'This is the opening text of the movie',
-//     releaseDate: '2021-05-18',
-//   },
-//   {
-//     id: 2,
-//     title: 'Some Dummy Movie 2',
-//     openingText: 'This is the second opening text of the movie',
-//     releaseDate: '2021-05-19',
-//   },
-// ];
-
-const transformMovieData = (data = []) =>
-  data.results.map((movie, i) => ({
-    id: movie.episode_id,
-    title: movie.title,
-    openingText: movie.opening_crawl,
-    releaseDate: movie.release_date,
-  }));
+const BASE_URL =
+  "https://react-tutorial-2832d-default-rtdb.asia-southeast1.firebasedatabase.app";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const generateMovies = useCallback(async () => {
-    setLoading(true);
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch(`${BASE_URL}/films`, {
-        method: "GET",
-      });
-
+      const response = await fetch(`${BASE_URL}/movies.json`);
       if (!response.ok) {
-        throw new Error("Something went wrong...");
+        throw new Error("Something went wrong!");
       }
 
       const data = await response.json();
-      const transformedData = transformMovieData(data);
 
-      setMovies((prev) => (prev = [...transformedData]));
+      const loadedMovies = [];
 
-      setLoading(false);
-    } catch (e) {
-      setError(e.message);
-      setLoading(false);
+      for (const key in data) {
+        const movieObj = {
+          id: key,
+          ...data[key],
+        };
+
+        loadedMovies.push(movieObj);
+      }
+
+      setMovies(loadedMovies);
+    } catch (error) {
+      setError(error.message);
     }
+    setIsLoading(false);
   }, []);
 
   useEffect(() => {
-    generateMovies();
-  }, [generateMovies]);
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  async function addMovieHandler(movie) {
+    try {
+      const res = await fetch(`${BASE_URL}/movies.json`, {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={generateMovies}>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        {loading && <p>Loading...</p>}
-        {!loading && movies.length === 0 && !error && <p>No movies...</p>}
-        {!loading && error !== null && <p>{error}</p>}
-        {!loading && movies.length > 0 && <MoviesList movies={movies} />}
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
