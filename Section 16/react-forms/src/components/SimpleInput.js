91d824b795824341804c15248c09e58a37c2c7c9
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-10-24 09:52:15 +0800

    Validating forms

--- Section 16/react-forms/src/components/SimpleInput.js
+++ Section 16/react-forms/src/components/SimpleInput.js
@@ -1,12 +1,93 @@
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+  const [name, setName] = useState("");
+  const [nameTouched, setNameTouched] = useState(false);
+
+  const [email, setEmail] = useState("");
+  const [emailTouched, setEmailTouched] = useState(false);
+
+  const nameIsValid = name.length > 0;
+  const nameIsNotValid = !nameIsValid && nameTouched;
+
+  const emailIsValid = email.length > 0 && email.includes("@");
+  const emailIsNotValid = !emailIsValid && emailTouched;
+
+  const inputFields = [
+    {
+      id: "name",
+      type: "text",
+      label: "Your name",
+      value: name,
+      isValid: !nameIsNotValid,
+      change: (e) => {
+        setName(e.target.value);
+      },
+      blur: (e) => {
+        setNameTouched(true);
+      },
+      errorMessage: "Name must not be empty",
+    },
+    {
+      id: "email",
+      type: "email",
+      label: "Your email",
+      value: email,
+      isValid: !emailIsNotValid,
+      change: (e) => {
+        setEmail(e.target.value);
+      },
+      blur: (e) => {
+        setEmailTouched(true);
+      },
+      errorMessage: "Invalid email address",
+    },
+  ];
+
+  let formIsValid = true;
+
+  if (nameIsValid && emailIsValid) formIsValid = true;
+  else formIsValid = false;
+
+  const submitFormAction = (e) => {
+    e.preventDefault();
+
+    setNameTouched(true);
+    setEmailTouched(true);
+
+    if (!nameIsValid && !emailIsValid) return;
+
+    const data = { name, email };
+    console.log("Data: ", JSON.stringify(data));
+    setNameTouched(false);
+    setEmailTouched(false);
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
-      </div>
+    <form onSubmit={submitFormAction}>
+      {inputFields.map(
+        ({ change, blur, isValid, label, type, value, id, errorMessage }) => (
+          <div
+            className={isValid ? "form-control" : "form-control invalid"}
+            key={id}
+          >
+            <label htmlFor={id}>{label}</label>
+            <input
+              type={type}
+              id={id}
+              value={value}
+              onChange={change}
+              onBlur={blur}
+            />
+            {!isValid && <p className="error-text">{errorMessage}</p>}
+          </div>
+        )
+      )}
+
       <div className="form-actions">
-        <button>Submit</button>
+        <button type="submit" disabled={!formIsValid}>
+          Submit
+        </button>
       </div>
     </form>
   );
