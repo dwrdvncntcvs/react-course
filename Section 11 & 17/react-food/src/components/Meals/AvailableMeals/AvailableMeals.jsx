59a76551e559f2c12691841f4c294b2ac59382ae
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-10-26 09:29:47 +0800

    Fetching, loading and handling error

--- Section 11 & 17/react-food/src/components/Meals/AvailableMeals/AvailableMeals.jsx
+++ Section 11 & 17/react-food/src/components/Meals/AvailableMeals/AvailableMeals.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 import style from "./availableMeals.module.css";
-import { DUMMY_MEALS } from "../../../data/data-meals";
 import { Card } from "../../UI";
 import MealItem from "../MealItem/MealItem";
+import { useState } from "react";
+import { useEffect } from "react";
+import { BASE_URL } from "../../../utils/variables";
+import { useCallback } from "react";
+import useHttp from "../../../hooks/useHttp";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(({ name, description, price, id }) => (
+  const [meals, setMeals] = useState([]);
+  const { error, loading, request } = useHttp();
+
+  const transformData = useCallback((data) => {
+    const mealsData = [];
+
+    for (let key in data) {
+      mealsData.push({ id: key, ...data[key] });
+    }
+
+    setMeals(mealsData);
+  }, []);
+
+  useEffect(() => {
+    request(`${BASE_URL}/meals.json`, {}, transformData);
+  }, [transformData, request]);
+
+  const mealsList = meals.map(({ name, description, price, id }) => (
     <MealItem
       name={name}
       description={description}
@@ -18,7 +39,8 @@ const AvailableMeals = () => {
   return (
     <section className={style.meals}>
       <Card>
-        <ul>{mealsList}</ul>
+        {!loading ? <ul>{mealsList}</ul> : <p>Loading...</p>}
+        {error.length > 0 && !loading && <p>{error}</p>}
       </Card>
     </section>
   );
---
+++ Section 11 & 17/react-food/src/hooks/useHttp.js
@@ -0,0 +1,27 @@
+import { useCallback, useState } from "react";
+
+const useHttp = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const request = useCallback(async (url, opts, setter) => {
+    setLoading(true);
+    const response = await fetch(url, { ...opts });
+
+    if (!response.ok) {
+      if (response.status === 404) {
+        setError("Failed to load meals...");
+        setLoading(false);
+      }
+      return;
+    }
+
+    const responseData = await response.json();
+    setter(responseData);
+    setLoading(false);
+  }, []);
+
+  return { request, loading, error };
+};
+
+export default useHttp;
