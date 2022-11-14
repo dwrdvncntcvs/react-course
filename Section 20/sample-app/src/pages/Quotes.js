1523b191f9571b769e6765c8fdfb2b2fd55757d4
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-11-14 09:17:43 +0800

    Dummy List ++

--- Section 20/sample-app/src/pages/Quotes.js
+++ Section 20/sample-app/src/pages/Quotes.js
@@ -1,11 +1,25 @@
 import React from "react";
 import QuoteList from "../components/quotes/QuoteList";
+import uuid from "react-uuid";
+
+const DUMMY_QUOTES = [
+  {
+    id: uuid(),
+    author: "Edward Vincent Cuevas",
+    text: "Hello World",
+  },
+  {
+    id: uuid(),
+    author: "Edward Vincent Cuevas",
+    text: "I'am, inevitable",
+  },
+];
 
 export default function Quotes() {
   return (
     <div>
       <h1>Quotes</h1>
-      <QuoteList quotes={[]} />
+      <QuoteList quotes={DUMMY_QUOTES} />
     </div>
   );
 }
