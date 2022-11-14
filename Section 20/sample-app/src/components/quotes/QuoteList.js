3e30679ac9566ca61c4a72c29498d0f5986b8a34
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-11-14 18:06:25 +0800

    Sorting

--- Section 20/sample-app/src/components/quotes/QuoteList.js
+++ Section 20/sample-app/src/components/quotes/QuoteList.js
@@ -1,13 +1,31 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
+import { useSearchParams } from "react-router-dom";
+import { sortQuotes } from "../../helper/helper";
 
-import QuoteItem from './QuoteItem';
-import classes from './QuoteList.module.css';
+import QuoteItem from "./QuoteItem";
+import classes from "./QuoteList.module.css";
 
 const QuoteList = (props) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const sort = searchParams.get("sort");
+  const isSortingAsc = sort === "asc";
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAsc);
+
+  const changeSorting = () => {
+    setSearchParams({ sort: isSortingAsc ? "dsc" : "asc" });
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSorting}>
+          Sort {isSortingAsc ? "Descending" : "Ascending"}
+        </button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
---
+++ Section 20/sample-app/src/helper/helper.js
@@ -0,0 +1,9 @@
+export const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
