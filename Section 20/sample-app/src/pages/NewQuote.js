c82862548ff3fbf749e4934dc4a6a399ebf52dfb
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-11-14 17:52:14 +0800

    Minor changes

--- Section 20/sample-app/src/pages/NewQuote.js
+++ Section 20/sample-app/src/pages/NewQuote.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 
 function NewQuote() {
+  const navigate = useNavigate();
+
   const addQuoteHandler = (data) => {
     console.log("Quote Data: ", data);
+    navigate({ pathname: "/quotes" });
   };
 
   return (
