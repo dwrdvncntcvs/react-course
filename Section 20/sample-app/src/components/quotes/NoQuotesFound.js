c344c72199183a1b7b2853cd8a18fff3c53dc4da
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-11-14 17:40:13 +0800

    Minor changes

--- Section 20/sample-app/src/components/quotes/NoQuotesFound.js
+++ Section 20/sample-app/src/components/quotes/NoQuotesFound.js
@@ -1,12 +1,11 @@
-import classes from './NoQuotesFound.module.css';
+import classes from "./NoQuotesFound.module.css";
+import { Link } from "react-router-dom";
 
 const NoQuotesFound = () => {
   return (
     <div className={classes.noquotes}>
       <p>No quotes found!</p>
-      <a className='btn'>
-        Add a Quote
-      </a>
+      <Link className="btn">Add a Quote</Link>
     </div>
   );
 };
