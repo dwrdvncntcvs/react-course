ed0a49a9100354d269809713467e999c0f1528ae
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-11-14 08:56:33 +0800

    Use of outlet

--- Section 20/sample-app/src/App.js
+++ Section 20/sample-app/src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import Comments from "./components/comments/Comments";
 import NewQuote from "./pages/NewQuote";
 import QuoteDetails from "./pages/QuoteDetails";
 import Quotes from "./pages/Quotes";
@@ -7,7 +8,9 @@ function App() {
   return (
     <Routes>
       <Route path="/quotes" index element={<Quotes />} />
-      <Route path="/quotes/:quoteId" element={<QuoteDetails />} />
+      <Route path="/quotes/:quoteId/" element={<QuoteDetails />}>
+        <Route path="comments" element={<Comments />} />
+      </Route>
       <Route path="/new-quote" element={<NewQuote />} />
       <Route path="*" element={<Navigate to="/quotes" replace />} />
     </Routes>
--- Section 20/sample-app/src/pages/QuoteDetails.js
+++ Section 20/sample-app/src/pages/QuoteDetails.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { useParams, Outlet } from "react-router-dom";
 
 export default function QuoteDetails() {
-  return <div>QuoteDetails</div>;
+  const { quoteId } = useParams();
+
+  return (
+    <div>
+      <h1>QuoteDetails</h1>
+      <p>{quoteId}</p>
+      <Outlet />
+    </div>
+  );
 }
