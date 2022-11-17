fe2dac5a76cc689fe3f5df8c49d334404a301381
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-11-17 14:44:19 +0800

    Redirect depends on auth state

--- Section 22/react-auth/src/App.js
+++ Section 22/react-auth/src/App.js
@@ -1,22 +1,31 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from "react-router-dom";
 
-import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import { useAuth } from "./hooks/useAuth";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
+  const { isLoggedIn } = useAuth();
+
   return (
     <Layout>
       <Switch>
-        <Route path='/' exact>
+        <Route path="/" exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
-          <AuthPage />
-        </Route>
-        <Route path='/profile'>
-          <UserProfile />
+        {!isLoggedIn ? (
+          <Route path="/auth">
+            <AuthPage />
+          </Route>
+        ) : (
+          <Route path="/profile">
+            <UserProfile />
+          </Route>
+        )}
+        <Route path="*">
+          <Redirect to="/" />
         </Route>
       </Switch>
     </Layout>
--- Section 22/react-auth/src/components/Layout/MainNavigation.js
+++ Section 22/react-auth/src/components/Layout/MainNavigation.js
@@ -1,10 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
 
 import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
   const { isLoggedIn, logout } = useAuth();
+  const history = useHistory();
+
+  const logoutAction = () => {
+    logout();
+    history.replace("/auth");
+  };
 
   return (
     <header className={classes.header}>
@@ -19,7 +25,7 @@ const MainNavigation = () => {
                 <Link to="/profile">Profile</Link>
               </li>
               <li>
-                <button onClick={logout}>Logout</button>
+                <button onClick={logoutAction}>Logout</button>
               </li>
             </>
           ) : (
