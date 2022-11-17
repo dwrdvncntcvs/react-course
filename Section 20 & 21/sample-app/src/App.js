da3c378bf8644ef10a1df24913ca7cab45ea46de
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-11-17 12:40:45 +0800

    Lazy loading added

--- Section 20 & 21/sample-app/src/App.js
+++ Section 20 & 21/sample-app/src/App.js
@@ -1,25 +1,36 @@
-import { Routes, Route, Navigate, Link } from "react-router-dom";
+import React, { Suspense } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Comments from "./components/comments/Comments";
 import Layout from "./components/layout/Layout";
 import CommentsLink from "./components/Links/CommentsLink";
-import NewQuote from "./pages/NewQuote";
-import NotFound from "./pages/NotFound";
-import QuoteDetails from "./pages/QuoteDetails";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
 import Quotes from "./pages/Quotes";
+
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+const QuoteDetails = React.lazy(() => import("./pages/QuoteDetails"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
 
 function App() {
   return (
     <Layout>
-      <Routes>
-        <Route path="/quotes" index element={<Quotes />} />
-        <Route path="/quotes/:quoteId/" element={<QuoteDetails />}>
-          <Route path="" element={<CommentsLink />} />
-          <Route path="comments" element={<Comments />} />
-        </Route>
-        <Route path="/new-quote" element={<NewQuote />} />
-        <Route path="/" element={<Navigate to="/quotes" replace />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/quotes" index element={<Quotes />} />
+          <Route path="/quotes/:quoteId/" element={<QuoteDetails />}>
+            <Route path="" element={<CommentsLink />} />
+            <Route path="comments" element={<Comments />} />
+          </Route>
+          <Route path="/new-quote" element={<NewQuote />} />
+          <Route path="/" element={<Navigate to="/quotes" replace />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </Layout>
   );
 }
