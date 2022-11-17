b9bd3a7c81ffc6c7e4562e7585a5ccb4811a5287
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-11-17 13:44:54 +0800

    Authentication with firebasee

--- Section 22/react-auth/src/components/Auth/AuthForm.js
+++ Section 22/react-auth/src/components/Auth/AuthForm.js
@@ -1,34 +1,70 @@
-import { useState } from 'react';
+import { useRef, useState } from "react";
+import { FIREBASE_API_KEY } from "../../utils/variables";
 
-import classes from './AuthForm.module.css';
+import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
   const [isLogin, setIsLogin] = useState(true);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitData = async (e) => {
+    e.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    if (isLogin) {
+    } else {
+      const authData = {
+        email,
+        password,
+        returnSecureToken: true,
+      };
+
+      const responseData = await fetch(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${FIREBASE_API_KEY}`,
+        {
+          method: "POST",
+          body: JSON.stringify(authData),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (!responseData.ok) {
+      }
+
+      const data = await responseData.json();
+      console.log("Data: ", data);
+    }
+  };
+
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitData}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" ref={emailRef} required />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input type="password" id="password" ref={passwordRef} required />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          <button>{isLogin ? "Login" : "Create Account"}</button>
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
---
+++ Section 22/react-auth/src/utils/variables.js
@@ -0,0 +1,5 @@
+const variable = {
+  FIREBASE_API_KEY: "AIzaSyD4hIY1q99Db2HqsUU2aFr2V5MT6QAL4Qw",
+};
+
+export const { FIREBASE_API_KEY } = variable;
