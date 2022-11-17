b7c9f90aeafa267c2d708af5f6c8ebde4455bf49
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-11-17 21:00:14 +0800

    Routing

--- Section 23/next-app/pages/news/index.tsx
+++ Section 23/next-app/pages/news/index.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import Link from "next/link";
 
 export default function NewsPage() {
-  return <div>news</div>;
+  return (
+    <>
+      <h1>News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-great">NextJS is great</Link>
+        </li>
+        <li>Something else</li>
+      </ul>
+    </>
+  );
 }
