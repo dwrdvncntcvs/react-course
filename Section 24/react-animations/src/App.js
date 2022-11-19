e874d2dae1bc5b7c7d7979a7ad8b5ba74f84cca3
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-11-19 15:57:16 +0800

    Conditionally managing animations

--- Section 24/react-animations/src/App.js
+++ Section 24/react-animations/src/App.js
@@ -22,8 +22,10 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal onClose={this.hideModal} show={this.state.modalIsOpen} />
-        <Backdrop show={this.state.modalIsOpen} />
+        {this.state.modalIsOpen && (
+          <Modal onClose={this.hideModal} show={this.state.modalIsOpen} />
+        )}
+        {this.state.modalIsOpen && <Backdrop show={this.state.modalIsOpen} />}
         <button className="Button" onClick={this.showModal}>
           Open Modal
         </button>
