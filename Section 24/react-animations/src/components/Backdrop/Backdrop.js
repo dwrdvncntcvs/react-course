19b4887dbe7dcd1fe205d22feb51fcdd26cf7d7b
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-11-19 15:40:08 +0800

    modals

--- Section 24/react-animations/src/components/Backdrop/Backdrop.js
+++ Section 24/react-animations/src/components/Backdrop/Backdrop.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import './Backdrop.css';
+import "./Backdrop.css";
 
-const backdrop = (props) => (
-    <div className="Backdrop"></div>
-);
+const backdrop = ({ show }) => {
+  const cssClasses = ["Backdrop", show ? "BackdropOpen" : "BackdropClose"];
+
+  return <div className={cssClasses.join(" ")}></div>;
+};
 
 export default backdrop;
--- Section 24/react-animations/src/components/Modal/Modal.js
+++ Section 24/react-animations/src/components/Modal/Modal.js
@@ -2,13 +2,17 @@ import React from "react";
 
 import "./Modal.css";
 
-const modal = (props) => (
-  <div className="Modal">
-    <h1>A Modal</h1>
-    <button className="Button" onClick={props.closed}>
-      Dismiss
-    </button>
-  </div>
-);
+const modal = ({ show, onClose }) => {
+  const cssClasses = ["Modal", show ? "ModalOpen" : "ModalClose"];
+
+  return (
+    <div className={cssClasses.join(" ")}>
+      <h1>A Modal</h1>
+      <button className="Button" onClick={onClose}>
+        Dismiss
+      </button>
+    </div>
+  );
+};
 
 export default modal;
