b2e7142ac8ccacc76ec49a0864db34e3f0cc462a
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-11-19 11:37:26 +0800

    Minor changes

--- Section 23/meetup-app/database/mongodb/config.js
+++ Section 23/meetup-app/database/mongodb/config.js
@@ -1,6 +1,7 @@
 import mongoose, { connect } from "mongoose";
 
-const MONGODB_URI = "mongodb://localhost/next_db";
+const MONGODB_URI =
+  "mongodb+srv://mongoAdmin:Pizza143@cluster0.wicwvkr.mongodb.net/?retryWrites=true&w=majority";
 
 async function dbConnect() {
   return await mongoose.connect(MONGODB_URI);
