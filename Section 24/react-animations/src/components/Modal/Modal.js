e0a1c2ca4beb52a71d7b7fab6aea2399ffcce5f7
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-11-19 16:36:37 +0800

    CssTransition

--- Section 24/react-animations/src/components/Modal/Modal.js
+++ Section 24/react-animations/src/components/Modal/Modal.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Transition } from "react-transition-group";
+import { CSSTransition } from "react-transition-group";
 
 import "./Modal.css";
 
@@ -10,27 +10,20 @@ const animationTiming = {
 
 const modal = ({ show, onClose }) => {
   return (
-    <Transition in={show} timeout={animationTiming} mountOnEnter unmountOnExit>
-      {(state) => {
-        const cssClasses = [
-          "Modal",
-          state === "entering"
-            ? "ModalOpen"
-            : state === "exiting"
-            ? "ModalClose"
-            : null,
-        ];
-
-        return (
-          <div className={cssClasses.join(" ")}>
-            <h1>A Modal</h1>
-            <button className="Button" onClick={onClose}>
-              Dismiss
-            </button>
-          </div>
-        );
-      }}
-    </Transition>
+    <CSSTransition
+      in={show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+      classNames={"fade-slide"}
+    >
+      <div className="Modal">
+        <h1>A Modal</h1>
+        <button className="Button" onClick={onClose}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
   );
 };
 
