12c1cc31114adb0f9ca53ecb629cbcfb50d2886c
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-11-24 09:32:10 +0800

    Functional Component generic type

---
+++ Section 27/react-ts/src/components/Todo.tsx
@@ -0,0 +1,18 @@
+import React, { FC, PropsWithChildren } from "react";
+import { Todo as TodoInterface } from "../interfaces/todo";
+
+interface TodoProps {
+  todo: TodoInterface[];
+}
+
+const Todo: FC<TodoProps & PropsWithChildren> = ({ todo }) => {
+  return (
+    <ul>
+      {todo.map(({ id, value }) => (
+        <li key={id}>{value}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default Todo;
