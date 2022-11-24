051e32c42cb34f067a177da8a0f112ac0f1083df
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-11-24 20:00:03 +0800

    Handling form input states

--- Section 28/react-hooks/src/components/Ingredients/IngredientForm.js
+++ Section 28/react-hooks/src/components/Ingredients/IngredientForm.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import Card from '../UI/Card';
-import './IngredientForm.css';
+import Card from "../UI/Card";
+import "./IngredientForm.css";
 
-const IngredientForm = React.memo(props => {
-  const submitHandler = event => {
+const IngredientForm = React.memo(() => {
+  const [inputData, setInputData] = useState({ title: "", amount: "" });
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setInputData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const submitHandler = (event) => {
     event.preventDefault();
     // ...
+
+    console.table(inputData);
   };
 
   return (
@@ -15,11 +26,23 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input
+              type="text"
+              id="title"
+              value={inputData.title}
+              name="title"
+              onChange={handleChange}
+            />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input
+              type="number"
+              id="amount"
+              value={inputData.amount}
+              name="amount"
+              onChange={handleChange}
+            />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
