74bdaa29d5c8517a501bbca9187351c49d9f4900
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2023-02-16 11:05:31 +0800

    Create linke to navigate from home to product page

--- Building a Multi-Page SPA with React Router/react-routing-app/src/pages/Home.jsx
+++ Building a Multi-Page SPA with React Router/react-routing-app/src/pages/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <p>This is the Home Page</p>
+      <p>
+        Go to <Link to="/products">Products</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Home;
