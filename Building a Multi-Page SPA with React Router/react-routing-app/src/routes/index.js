5e655e662d4ba12c3357463de86f75ba99bce90c
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2023-02-16 11:01:03 +0800

    Add product page

--- Building a Multi-Page SPA with React Router/react-routing-app/src/routes/index.js
+++ Building a Multi-Page SPA with React Router/react-routing-app/src/routes/index.js
@@ -1,4 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
+import Product from "../pages/Product";
 
-export const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
+export const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/products", element: <Product /> },
+]);
