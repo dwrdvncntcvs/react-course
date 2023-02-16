a9a15cdb936e5e32ac59ff0bf30cb66d4a76bbdb
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2023-02-16 11:45:28 +0800

    Use useParams

--- Building a Multi-Page SPA with React Router/react-routing-app/src/pages/ProductDetails.jsx
+++ Building a Multi-Page SPA with React Router/react-routing-app/src/pages/ProductDetails.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+  const { id: productId } = useParams();
+
+  return (
+    <div>
+      <h1>Product Details</h1>
+      <p>Product ID: {productId}</p>
+    </div>
+  );
 };
 
 export default ProductDetails;
