3c5849ede445ae21290cb42cb8c99ac5bf8f1c6c
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2023-02-16 11:22:58 +0800

    Add error element

---
+++ Building a Multi-Page SPA with React Router/react-routing-app/src/pages/NotFound.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import MainNavigation from "../components/MainNavigation";
+
+const NotFound = () => {
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        <h1>404 Error</h1>
+      </main>
+    </>
+  );
+};
+
+export default NotFound;
--- Building a Multi-Page SPA with React Router/react-routing-app/src/routes/index.js
+++ Building a Multi-Page SPA with React Router/react-routing-app/src/routes/index.js
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "../layouts/Root";
 import Home from "../pages/Home";
+import NotFound from "../pages/NotFound";
 import Product from "../pages/Product";
 
 export const router = createBrowserRouter([
   {
     path: "/",
+    errorElement: <NotFound />,
     element: <RootLayout />,
     children: [
       { path: "/", element: <Home /> },
