d39ff9912de5f4c8dfbc6b0ca584ad8aa1081311
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2023-02-16 10:51:02 +0800

    Add router provider with context on app

--- Building a Multi-Page SPA with React Router/react-routing-app/src/App.js
+++ Building a Multi-Page SPA with React Router/react-routing-app/src/App.js
@@ -1,5 +1,8 @@
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ Building a Multi-Page SPA with React Router/react-routing-app/src/routes/index.js
@@ -0,0 +1,4 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home";
+
+export const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
